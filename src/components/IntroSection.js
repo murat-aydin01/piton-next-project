6877e856f7f961b84280d0b88e431d0dddce0169
murat-aydin01 <murataydin2001@outlook.com>
2025-01-31 22:41:48 +0300

    intro düzenleme

--- src/components/IntroSection.js
+++ src/components/IntroSection.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function IntroSection() {
   return (
-    <section className="flex flex-col px-24 gap-32">
-      <div>
-        <h2>Who we are</h2>
-        <p>
+    <section className="flex flex-col justify-between px-[6vw] gap-y-[10vh]">
+      <div className="flex flex-col gap-y-8">
+        <h2 className="text-5xl font-bold">Who we are</h2>
+        <p className="text-3xl font-normal">
           We love what we do and create partnerships with our clients to ensure
           their digital transformation is positioned for long-term success.We
           believe that the human dimensions essential to start any successful
