831c81718e010801ea17ec72fe4b1a33533d0e63
murat-aydin01 <murataydin2001@outlook.com>
2025-01-31 21:56:08 +0300

    hero section responsive

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,13 +1,16 @@
 function HeroSection() {
   return (
-    <section className=" h-screen w-screen bg-[url('/images/HeroShape.png')] bg-cover bg-center bg-heroBg flex flex-col justify-between items-center ">
-      <h1 className="w-full mt-[10vw] text-8xl px-72 text-center ">Let’s create something great together.</h1>
-      <div className="flex px-32 w-full">
-      <img src="/images/Hands1.png" alt="hands 1" className="w-1/2"/>
-      <img src="/images/Hands2.png" alt="hands 2" className="w-1/2"/>
+    <section className="h-[40vh] md:h-[60vh] lg:h-[90vh] w-screen bg-[url('/images/HeroShape.png')] bg-cover bg-center bg-heroBg flex flex-col justify-between items-center">
+      <h1 className="w-full my-auto text-center px-[20%] 
+                 text-3xl sm:text-4xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl">
+    Let’s create something great together.
+  </h1>
+      <div className="flex px-[10%] w-full h- justify-center md:justify-between">
+        <img src="/images/Hands1.png" alt="hands 1" className="w-1/2 " />
+        <img src="/images/Hands2.png" alt="hands 2" className="  w-1/2" />
       </div>
     </section>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
