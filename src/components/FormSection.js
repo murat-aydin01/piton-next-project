71be07a75d5362d8f9e8686aa89e1c734161e6b8
murat-aydin01 <murataydin2001@outlook.com>
2025-02-01 00:01:05 +0300

    form section düzenlemeler

--- src/components/FormSection.js
+++ src/components/FormSection.js
@@ -4,16 +4,19 @@ import useContactForm from "@/hooks/useContactForm";
 
 function FormSection() {
   const { formData, handleChange, handleSubmit } = useContactForm();
+
   return (
-    <section className="flex flex-row justify-around">
-      <div className="w-4/12">
-        <img src="/images/SideImage.png" alt="side image" />
+    <section className="flex flex-col md:flex-row justify-around items-center flex-wrap px-4 md:px-8 lg:px-24 py-14">
+      <div className="w-full md:w-4/12 mb-8 md:mb-0">
+        <img src="/images/SideImage.png" alt="side image" className="w-full h-auto" />
       </div>
-      <div className="flex flex-col w-5/12">
-        <h2 className="self-end">We&apos;d love to hear from you</h2>
+      <div className="flex flex-col w-full md:w-5/12">
+        <h2 className="self-center md:self-end text-4xl font-bold mb-8">
+          We&apos;d love to hear from you
+        </h2>
         <form className="flex flex-col gap-y-8" onSubmit={handleSubmit}>
           <input
-            className="border-2 border-inputBorder "
+            className="border-2 border-inputBorder h-24 p-4"
             type="text"
             name="name"
             value={formData.name}
@@ -21,7 +24,7 @@ function FormSection() {
             onChange={handleChange}
           />
           <input
-          className="border-2 border-inputBorder"
+            className="border-2 border-inputBorder h-24 p-4"
             type="email"
             name="email"
             value={formData.email}
@@ -29,7 +32,7 @@ function FormSection() {
             onChange={handleChange}
           />
           <input
-          className="border-2 border-inputBorder"
+            className="border-2 border-inputBorder h-24 p-4"
             type="url"
             name="url"
             value={formData.url}
@@ -37,14 +40,16 @@ function FormSection() {
             onChange={handleChange}
           />
           <input
-          className="border-2 border-inputBorder"
+            className="border-2 border-inputBorder h-48 p-4"
             type="text"
             name="details"
             value={formData.details}
             placeholder="Project Details*"
             onChange={handleChange}
           />
-          <button className="bg-black text-white mt-4" type="submit">Send Proposal</button>
+          <button className="bg-black text-white mt-4 h-24" type="submit">
+            Send Proposal
+          </button>
         </form>
       </div>
     </section>
