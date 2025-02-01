bf3449bd5b6105a60d5b506d8a8e7f84642d9b18
murat-aydin01 <murataydin2001@outlook.com>
2025-02-01 16:32:25 +0300

    intro seciton space-y ve text

--- src/components/IntroSection.js
+++ src/components/IntroSection.js
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function IntroSection() {
   return (
-<section className="snap-start flex flex-col justify-center px-4 sm:px-[6vw] gap-y-6 xl:min-h-screen py-20">
+<section className="snap-start flex flex-col justify-center px-4 sm:px-[6vw] gap-y-6 lg:gap-y-24 xl:min-h-screen py-20">
       <div className="flex flex-col gap-y-4 sm:gap-y-6">
         <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold">
           Who we are
         </h2>
-        <p className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-">
+        <p className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
           We love what we do and create partnerships with our clients to ensure
           their digital transformation is positioned for long-term success. We
           believe that the human dimensions are essential to start any successful
@@ -15,7 +15,7 @@ export default function IntroSection() {
           the company and people are born.
         </p>
       </div>
-      <img src="/images/MidImage.png" alt="Middle Image" className="w-full max-w-6xl self-center h-auto" />
+      <img src="/images/MidImage.png" alt="Middle Image" className="w-full h-auto" />
     </section>
   );
 }
